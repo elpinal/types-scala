5174782fda1b703eccb43b56f6743faf741e5eb8
elpinal <elpinal@users.noreply.github.com>
2018-01-10 23:15:33 +0900

    Introduce Types trait

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -1,14 +1,19 @@
-sealed abstract class Type
+trait Types[T] {
+        def subst(s: String => Type): T
+}
+
+sealed abstract class Type extends Types[Type] {
+        def subst(s: String => Type): Type = this match {
+                case Var(i) => s(i)
+                case Int() => Int()
+                case Arr(ty1, ty2) => Arr(ty1.subst(s), ty2.subst(s))
+        }
+}
+
 case class Var(i: String) extends Type
-case object Int extends Type
+case class Int() extends Type
 case class Arr(ty1: Type, ty2: Type) extends Type
 
 object Main extends App {
-        val subst: (String => Type, Type) => Type = (s: String => Type, ty: Type) => ty match {
-                case Var(i) => s(i)
-                case Int => Int
-                case Arr(ty1, ty2) => Arr(subst(s, ty1), subst(s, ty2))
-        }
-
         println("Hello, Scala!")
 }
