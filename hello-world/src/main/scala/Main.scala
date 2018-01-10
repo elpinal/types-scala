cf12077fd6d4839440f431fe560961752d95cd2c
elpinal <elpinal@users.noreply.github.com>
2018-01-10 22:41:49 +0900

    Add Type class

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -1,3 +1,8 @@
+sealed abstract class Type
+case class Var(i: String) extends Type
+case object Int extends Type
+case class Arr(ty1: Type, ty2: Type) extends Type
+
 object Main extends App {
-  println("Hello, World!")
+  println("Hello, Scala!")
 }
