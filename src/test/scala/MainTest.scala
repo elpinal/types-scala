f9d23bdef9d284452857ac7acb1b6fa8bb8abaed
elpinal <elpinal@users.noreply.github.com>
2018-01-11 19:33:10 +0900

    Test some functions

---
+++ src/test/scala/MainTest.scala
@@ -0,0 +1,21 @@
+import org.scalatest.FunSuite
+
+class MainTest extends FunSuite {
+  test("Type.subst") {
+    val s = Subst(Map("X" -> Type.Var("Y")))
+    val t1 = Type.Var("X")
+    val t2 = Type.Var("Y")
+    assert(t1.subst(s) == t2)
+
+    val t = Type.Var("Z")
+    assert(t.subst(s) == t)
+  }
+
+  test("ConstraintTyping.getTypeAndConstraint") {
+    val ctx = Context(List(Type.Var("A")))
+    val t = Term.Abs(Type.Var("B"), Term.App(Term.Var(0), Term.Var(1)))
+    val ty = Type.Arr(Type.Var("B"), Type.Var("v0"))
+    val cs = Constraint.set(Type.Var("B") -> Type.Arr(Type.Var("A"), Type.Var("v0")))
+    assert(ConstraintTyping.getTypeAndConstraint(ctx, t) == Right(ty, cs))
+  }
+}
