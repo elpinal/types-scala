6ba8952160c5f8b774a0827203e0c54835b53192
elpinal <elpinal@users.noreply.github.com>
2018-01-11 17:02:04 +0900

    Indent with 2 spaces

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -1,13 +1,13 @@
 trait Types[T] {
-        def subst(s: String => Type): T
+  def subst(s: String => Type): T
 }
 
 sealed abstract class Type extends Types[Type] {
-        def subst(s: String => Type): Type = this match {
-                case Var(i) => s(i)
-                case Int() => Int()
-                case Arr(ty1, ty2) => Arr(ty1.subst(s), ty2.subst(s))
-        }
+  def subst(s: String => Type): Type = this match {
+    case Var(i) => s(i)
+    case Int() => Int()
+    case Arr(ty1, ty2) => Arr(ty1.subst(s), ty2.subst(s))
+  }
 }
 
 case class Var(i: String) extends Type
@@ -15,5 +15,5 @@ case class Int() extends Type
 case class Arr(ty1: Type, ty2: Type) extends Type
 
 object Main extends App {
-        println("Hello, Scala!")
+  println("Hello, Scala!")
 }
