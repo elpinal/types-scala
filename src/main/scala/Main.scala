4aee9b5c8cff1900500cce3a54af357464d01fac
elpinal <elpinal@users.noreply.github.com>
2018-01-11 17:13:43 +0900

    Wrap Type class with Type object

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,24 @@
 trait Types[T] {
-  def subst(s: String => Type): T
+  def subst(s: String => Type.Type): T
 }
 
-sealed abstract class Type extends Types[Type] {
-  def subst(s: String => Type): Type = this match {
-    case Var(i) => s(i)
-    case Int() => Int()
-    case Arr(ty1, ty2) => Arr(ty1.subst(s), ty2.subst(s))
+object Type {
+  sealed abstract class Type extends Types[Type] {
+    def subst(s: String => Type): Type = this match {
+      case Var(i) => s(i)
+      case Int => Int
+      case Arr(ty1, ty2) => Arr(ty1.subst(s), ty2.subst(s))
+    }
   }
-}
 
-case class Var(i: String) extends Type
-case class Int() extends Type
-case class Arr(ty1: Type, ty2: Type) extends Type
+  case class Var(i: String) extends Type
+  case object Int extends Type
+  case class Arr(ty1: Type, ty2: Type) extends Type
+}
 
 object Main extends App {
   println("Hello, Scala!")
+  println("type variable", Type.Var("x"))
+  println("Int type", Type.Int)
+  println("x -> Int", Type.Arr(Type.Var("x"), Type.Int))
 }
