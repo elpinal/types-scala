75f78740c281a8c3207962ba2f9505c84cc8f0fe
elpinal <elpinal@users.noreply.github.com>
2018-01-12 11:59:19 +0900

    Test with tables

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,21 +1,30 @@
-import org.scalatest.FunSuite
+import org.scalatest._
+import prop._
 
-class MainTest extends FunSuite {
-  test("Type.subst") {
+class MainTest extends PropSpec with TableDrivenPropertyChecks with Matchers {
+  property("Type.subst") {
     val s = Subst(Map("X" -> Type.Var("Y")))
-    val t1 = Type.Var("X")
-    val t2 = Type.Var("Y")
-    assert(t1.subst(s) == t2)
-
     val t = Type.Var("Z")
-    assert(t.subst(s) == t)
-
     val t0 = Type.Var("v0")
-    val s1 = Subst(Map("X" -> Type.Arr(Type.Var("X"), Type.Var("X"))))
-    assert(t0.subst(s1) == t0)
+
+    val examples =
+      Table(
+        ("ty1", "s", "ty2"),
+        (Type.Var("X"), s, Type.Var("Y")),
+        (t, s, t),
+        (
+          t0,
+          Subst(Map("X" -> Type.Arr(Type.Var("X"), Type.Var("X")))),
+          t0
+        )
+      )
+
+    forAll(examples) { case (t1, s, t2) =>
+      t1.subst(s) should be (t2)
+    }
   }
 
-  test("ConstraintTyping.getTypeAndConstraint") {
+  property("ConstraintTyping.getTypeAndConstraint") {
     val ctx = Context(List(Type.Var("A")))
     val t = Term.Abs(Type.Var("B"), Term.App(Term.Var(0), Term.Var(1)))
     val ty = Type.Arr(Type.Var("B"), Type.Var("v0"))
@@ -23,14 +32,14 @@ class MainTest extends FunSuite {
     assert(ConstraintTyping.getTypeAndConstraint(ctx, t) == Right(ty, cs))
   }
 
-  test("unify Set of Constraint") {
+  property("unify Set of Constraint") {
     val t = Type.Arr(Type.Var("A"), Type.Var("v0"))
     val cs = Constraint.set(Type.Var("B") -> t)
     val s = Subst(Map("B" -> t))
     assert(Constraint.unify(cs) == Right(s))
   }
 
-  test("PrincipalType.fromTermWithContext") {
+  property("PrincipalType.fromTermWithContext") {
     val t = Term.App(
       Term.Abs(Type.Var("X"), Term.Var(0)),
       Term.Abs(Type.Var("Y"), Term.Var(0))
